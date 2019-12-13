611993aa89911a603007365bc02d37b6b019e656
Olivia Lund <recursory@Olivias-MacBook-Air.local>
2019-12-12 23:38:04 -0800

    add tests for piecesAt having correct bordersw

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -20,9 +20,22 @@ TEST(PiezasTest, toggleTurn)
   ASSERT_TRUE(p.toggleTurn() == O);
 }
 
-TEST(PiezasTest, pieceAtHappy)
+TEST(PiezasTest, pieceAtValid)
 {
   Piezas p;
   ASSERT_TRUE(p.pieceAt(0, 0) == Blank);
 }
 
+TEST(PiezasTest, pieceAtBelowX){
+  Piezas p;
+  ASSERT_TRUE(p.pieceAt(-1, 0) == Blank);
+}TEST(PiezasTest, pieceAtAboveX){
+  Piezas p;
+  ASSERT_TRUE(p.pieceAt(3, 0) == Blank);
+}TEST(PiezasTest, pieceAtBelowY{
+  Piezas p;
+  ASSERT_TRUE(p.pieceAt(0, -1) == Blank);
+}TEST(PiezasTest, pieceAtAboveY{
+  Piezas p;
+  ASSERT_TRUE(p.pieceAt(0, 3) == Blank);
+}
